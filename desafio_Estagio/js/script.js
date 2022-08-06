13ccc5a87eefafebac957becc8ce82a55ab5a1c0
makalau <marcelovictor95@hotmail.com>
2022-08-06 03:54:41 -0300

    Desafio vaga Estágio - E-commerce

---
+++ desafio_Estagio/js/script.js
@@ -0,0 +1,9 @@
+const frutas = document.querySelectorAll(frutas);
+const url = "/api/fruit/:nutrition?min=0&max=1000"
+fetch("/api/fruit/:nutrition?min=0&max=1000")
+.then(function(){
+
+})
+.catch(function(){
+
+});
